95e38e4c11ad229cd5b5cd5cbd44ae5edb218989
Losvapos69 <hectorcrackgaeta@gmail.com>
2026-03-28 22:31:06 -0600

    Update memorama.js

--- memorama.js
+++ memorama.js
@@ -3,7 +3,7 @@ let temas = {
     avengers: ["1.jpg","2.jpg","3.jpg","4.jpg","5.jpg","6.jpg","7.jpg","8.jpg"],
     animales: ["1.jpg","2.jpg","3.jpg","4.jpg","5.jpg","6.jpg","7.jpg","8.jpg"],
     comida: ["1.jpg","2.jpg","3.jpg","4.jpg","5.jpg","6.jpg","7.jpg","8.jpg"],
-    deportes: ["1.png","2.png","3.png","4.png","5.png","6.png","7.png","8.png"],
+    deportes: ["1.jpg","2.jpg","3.jpg","4.jpg","5.jpg","6.jpg","7.jpg","8.jpg"],
     carros: ["1.jpg","2.jpg","3.jpg","4.jpg","5.jpg","6.jpg","7.jpg","8.jpg"],
     logos: ["1.jpg","2.jpg","3.jpg","4.jpg","5.jpg","6.jpg","7.jpg","8.jpg"]
 };
